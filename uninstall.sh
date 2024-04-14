be627e0f9c76b03906a3c9675c114c03ab183f37
亀栗大也 <157256239+kamekuridaiya@users.noreply.github.com>
2024-04-14 18:17:45 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,6 @@
-sudo apt uninstall gnome-text-editor -y
-sudo apt uninstall gimp -y
-sudo apt uninstall nautilus -y
-sudo apt uninstall vlc -y
-sudo apt uninstall x11-apps -y
-cd /tmp
-sudo apt uninstall --fix-missing ./google-chrome-stable_current_amd64.deb
+sudo apt remove gnome-text-editor -y
+sudo apt remove gimp -y
+sudo apt remove nautilus -y
+sudo apt remove vlc -y
+sudo apt remove x11-apps -y
+sudo dpkg -r google-chrome-stable
