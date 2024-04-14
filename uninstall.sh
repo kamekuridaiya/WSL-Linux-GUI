a333aaac904041266891917366c1400f690ad753
亀栗大也 <157256239+kamekuridaiya@users.noreply.github.com>
2024-04-14 18:08:41 +0900

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+sudo apt uninstall gnome-text-editor -y
+sudo apt uninstall gimp -y
+sudo apt uninstall nautilus -y
+sudo apt uninstall vlc -y
+sudo apt uninstall x11-apps -y
+cd /tmp
+sudo apt uninstall --fix-missing ./google-chrome-stable_current_amd64.deb
