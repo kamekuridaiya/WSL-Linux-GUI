7dd174b30fd64e2fb24443fd6ce7b81855de97b9
亀栗大也 <157256239+kamekuridaiya@users.noreply.github.com>
2024-04-15 19:28:02 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+read -n 1 -s -r -p "This app installs basic GUI apps on WSL. Press any key to continue."
+echo "Installing..."
 sudo apt update
 sudo apt-get install wget
 sudo apt install gnome-text-editor -y
@@ -8,3 +10,4 @@ sudo apt install x11-apps -y
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install --fix-missing ./google-chrome-stable_current_amd64.deb
+echo "All done!"
